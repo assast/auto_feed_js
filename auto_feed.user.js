5633c9c2cde075bc0b747113a7cd6dee517d5dae
guotao <guotao09@126.com>
2024-11-07 13:51:03 +0800

    1

--- auto_feed.user.js
+++ auto_feed.user.js
@@ -92,7 +92,7 @@
 // @require      https://greasyfork.org/scripts/444988-music-helper/code/music-helper.js?version=1268106
 // @icon         https://kp.m-team.cc//favicon.ico
 // @run-at       document-end
-// @version      2.0.7.8
+// @version      1.0.0
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setClipboard
 // @grant        GM_setValue
